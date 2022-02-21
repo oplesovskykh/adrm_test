50e99a35758c5a786789d3ab83f4f1c4874ce31f
oplesovskykh <aleksandra.plesovskikh@gmail.com>
2022-02-21 11:37:27 +0200

    without class

--- test_1.py
+++ test_1.py
@@ -1,5 +1,10 @@
 import pytest
 
-def test_method_WPL30_T35():
+# def test_method_WPL30_T35():
+#     print("Hello World!")
+#     assert True
+
+
+def test_method_1_without_class():
     print("Hello World!")
     assert True
